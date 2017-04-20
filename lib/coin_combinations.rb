6faf53e476ac8ebc923f7766cea26c7ec25a62c1
Dana Weiss and Tyler Stephenson <dana@>
2017-04-20 09:52:09 -0700

    add test and logic for 1 penny

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -1,9 +1,21 @@
 class Fixnum
   define_method(:coin_combinations) do
-    result = ""
+    coins = Hash.new()
+    coins.store(25, "quarter")
+    coins.store(10, "dime")
+    coins.store(5, "nickel")
+    coins.store(1, "penny")
+    coin_array = []
+    result = coin_array.join(' ')
+    pennies = self./(1).to_s
+
     if self > 99
-      result = "No change needed"
+      coin_array.push("No change needed")
+    elsif self > 1
+      coin_array.push(pennies.+(" pennies"))
+    elsif self == 1
+      coin_array.push(pennies.+(" penny"))
     end
-    result
+    result = coin_array.join(' ')
   end
 end
--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -5,4 +5,8 @@ describe('Fixnum#coin_combinations') do
   it('will return no change needed if amount is greater than 99') do
     expect(100.coin_combinations()).to(eq("No change needed"))
   end
+
+  it('will return the proper amount of pennies') do
+    expect(1.coin_combinations()).to(eq('1 penny'))
+  end
 end
